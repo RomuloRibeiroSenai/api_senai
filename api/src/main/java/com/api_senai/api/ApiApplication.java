857b9cd48d479c1f6b7cf3c171f58812663672f3
RomuloRibeiroSenai <142239254+RomuloRibeiroSenai@users.noreply.github.com>
2023-10-04 11:46:56 -0300

    Add files via upload

--- api/src/main/java/com/api_senai/api/ApiApplication.java
+++ api/src/main/java/com/api_senai/api/ApiApplication.java
@@ -3,6 +3,8 @@ package com.api_senai.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.api_senai.api.entities.Endereco;
+
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -11,7 +13,7 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 
-		System.out.println("hello world");
+		Endereco.getEnderecoByCep("25645045");
 	}
 
 }
--- api/src/main/java/com/api_senai/api/entities/Endereco.java
+++ api/src/main/java/com/api_senai/api/entities/Endereco.java
@@ -1,5 +1,16 @@
 package com.api_senai.api.entities;
 
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import com.google.gson.Gson;
+
 import lombok.Data;
 
 //import org.apache.http.client.methods.HttpGet;
@@ -13,14 +24,30 @@ public class Endereco {
     private String localidade;
     private String uf;
 
-    public Endereco getEnderecoByCep(String cep){
-        Endereco endereco = new Endereco();
+    public static Endereco getEnderecoByCep(String cep){
+        Endereco local = new Endereco();
         /* mandar o cep pro via cep
         tratar a resposta
         montar o endereço
          */
         HttpGet request = new HttpGet("http://viacep.com.br/ws/"+ cep + "/json/");
 
-        return endereco;
+
+        try(CloseableHttpClient https = HttpClientBuilder.create().build();
+            CloseableHttpResponse resposta = https.execute(request))
+            {
+                HttpEntity entidade = resposta.getEntity();
+                String resultado = EntityUtils.toString(entidade);
+                System.out.println(resultado);
+
+                Gson gsonado = new Gson();
+                local = gsonado.fromJson(resultado, Endereco.class);
+                local.setComplemento(null);
+                System.out.println(local);
+            } catch (IOException abaa){
+            abaa.printStackTrace();
+        }
+
+        return local;
     }
 }
